02dbc195aab487b12cc8f21c8066c2e4f1fbc618
Kasper Gałkowski <k@galkowski.xyz>
2022-05-14 15:22:48 +0200

    trochę nowych zmian z emacsa 29

--- .config/emacs/modules/emacs29-module.el
+++ .config/emacs/modules/emacs29-module.el
@@ -3,3 +3,10 @@
 ;; Stuff that's coming in Emacs 29
 
 (setq help-window-keep-selected t) 
+
+(add-to-list
+ 'menu-bar-buffers-menu-command-entries
+ '(kill-current-buffer menu-item "Kill buffer" kill-current-buffer :help "Delete the current buffer")
+ t)
+
+(setq switch-to-prev-buffer-skip-regexp '("*Help* *inferior-lisp*"))
