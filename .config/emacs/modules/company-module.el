f81dbb6f435167a62e507e0623d5bb8715784bc2
Kasper Gałkowski <k@galkowski.xyz>
2022-05-28 03:20:06 +0200

    Dodałem komentarze do ustawień company

--- .config/emacs/modules/company-module.el
+++ .config/emacs/modules/company-module.el
@@ -5,9 +5,16 @@
 ;; Who would ever want a completion to get downcased?
 (setq company-dabbrev-downcase nil)
 
+;; Match case-insensitively
 (setq company-dabbrev-ignore-case t)
+
+;; Start completing after 2 characters
 (setq company-minimum-prefix-length 2)
+
+;; Enable quick match selection with M-{1..9}
 (setq company-show-numbers 'left)
+
+;; Don't clutter the modeline
 (setq company-lighter "")
 
 (autoload 'global-company-mode "company")
