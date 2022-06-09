9a0542036215bcf82c26d6947354a933ec6061e3
Kasper Gałkowski <k@galkowski.xyz>
2022-06-09 11:59:01 +0200

    Dodane podświetlanie innych komend (tak jak slime-compile-defun)

--- .config/emacs/modules/lisp-module.el
+++ .config/emacs/modules/lisp-module.el
@@ -7,3 +7,13 @@
   (modify-syntax-entry ?\] ")[" lisp-mode-syntax-table)
   (modify-syntax-entry ?\{ "(}" lisp-mode-syntax-table)
   (modify-syntax-entry ?\} "){" lisp-mode-syntax-table))
+
+;; Adds sexp flashing, a'la SLIME's C-c C-c, to other sexp evaluation commands
+(add-to-list 'load-path (concat +vendor-dir+ "eval-sexp-fu"))
+(autoload 'turn-on-eval-sexp-fu-flash-mode "eval-sexp-fu")
+(add-hook 'lisp-mode-hook 'turn-on-eval-sexp-fu-flash-mode)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eval-sexp-fu-flash-mode)
+(setq eval-sexp-fu-flash-duration 0.2)
+(setq eval-sexp-fu-flash-face 'secondary-selection)
+
+
