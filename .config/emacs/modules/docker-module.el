3c58ae009df4d4b7d5c682e199dd0e5b48042e64
Kasper Gałkowski <k@galkowski.xyz>
2022-09-21 23:48:20 +0200

    dodany docker-tramp

--- .config/emacs/modules/docker-module.el
+++ .config/emacs/modules/docker-module.el
@@ -3,3 +3,8 @@
 (add-to-list 'load-path (concat +vendor-dir+ "dockerfile-mode"))
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 (autoload 'dockerfile-mode "dockerfile-mode" "" t)
+
+(add-to-list 'load-path (concat +vendor-dir+ "docker-tramp"))
+(autoload 'docker-tramp "docker-tramp")
+(with-eval-after-load 'tramp
+  (require 'docker-tramp))
