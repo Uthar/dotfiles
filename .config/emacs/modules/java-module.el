9cc8f0ad306d90e522d757689545879a620bf966
Kasper Gałkowski <k@galkowski.xyz>
2022-10-09 14:29:31 +0200

    poprawka dla code actions jdtls
    see https://github.com/joaotavora/eglot/blob/7cbb6007cd042381198965824025ce07ec2ed9ff/eglot.el#L3050-L3055

--- .config/emacs/modules/java-module.el
+++ .config/emacs/modules/java-module.el
@@ -13,3 +13,8 @@
     (c-set-offset 'inexpr-class 0)
     ;; Don't indent members of the top-level class
     (c-set-offset 'inclass 'kaspi/c-lineup-java-toplevel-class)))
+
+(with-eval-after-load 'eglot
+  (cl-defmethod eglot-execute-command
+    (_server (_cmd (eql java.apply.workspaceEdit)) arguments)
+    (mapc #'eglot--apply-workspace-edit arguments)))
