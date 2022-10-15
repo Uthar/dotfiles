f7dfec4dd7e37fe837abeefcbfbff95a0565fdc2
Kasper Gałkowski <kgalkowski@comscore.com>
2022-10-15 20:28:53 +0200

    Usunąłem nieużywany kod

--- .config/emacs/modules/docker-module.el
+++ .config/emacs/modules/docker-module.el
@@ -3,8 +3,3 @@
 (add-to-list 'load-path (concat +vendor-dir+ "dockerfile-mode"))
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 (autoload 'dockerfile-mode "dockerfile-mode" "" t)
-
-(add-to-list 'load-path (concat +vendor-dir+ "docker-tramp"))
-(autoload 'docker-tramp "docker-tramp")
-(with-eval-after-load 'tramp
-  (require 'docker-tramp))
