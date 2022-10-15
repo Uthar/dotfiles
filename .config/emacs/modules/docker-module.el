aacfde5376f7b8a8ed44e6ea285deb5d00ead12d
Kasper Gałkowski <kgalkowski@comscore.com>
2022-10-15 20:50:23 +0200

    autoload kubel

--- .config/emacs/modules/docker-module.el
+++ .config/emacs/modules/docker-module.el
@@ -3,3 +3,5 @@
 (add-to-list 'load-path (concat +vendor-dir+ "dockerfile-mode"))
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 (autoload 'dockerfile-mode "dockerfile-mode" "" t)
+(add-to-list 'load-path (concat +vendor-dir+ "kubel"))
+(autoload 'kubel "kubel" "" t)
