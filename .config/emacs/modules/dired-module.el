e5648f0ba2485a85bf6590d4e7f027d733bc7a6d
Kasper Gałkowski <kgalkowski@comscore.com>
2022-10-15 20:57:20 +0200

    dodany consult

--- .config/emacs/modules/dired-module.el
+++ .config/emacs/modules/dired-module.el
@@ -30,5 +30,4 @@
     (local-set-key "j" 'dired-next-line)
     (local-set-key "k" 'dired-previous-line)
     (local-set-key "l" 'dired-find-file)
-    (local-set-key "h" 'dired-up-directory)
-    (local-set-key (kbd "C-s") 'kaspi/swiper-dired)))
+    (local-set-key "h" 'dired-up-directory)))
