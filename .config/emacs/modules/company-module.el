b9972dd0bd17e148d41a1145934038df211085e8
Kasper Gałkowski <kgalkowski@comscore.com>
2022-10-16 13:02:28 +0200

    napisałem samoodświeżające się complete-in-region

--- .config/emacs/modules/company-module.el
+++ .config/emacs/modules/company-module.el
@@ -23,3 +23,40 @@
 
 (global-set-key (kbd "C-M-<tab>") 'completion-at-point)
 (global-set-key (kbd "M-<tab>") 'company-complete)
+
+(setopt completions-format 'one-column)
+(setopt completion-auto-help 'visible)
+(setopt completion-show-help nil)
+(setopt display-buffer-alist '(("\\*Completions\\*"
+                                display-buffer-at-bottom
+                                (window-height . 0.25))))
+(add-to-list 'completion-styles 'flex)
+
+;;;;;;;; live completion-in-region
+
+(defvar lcr-timer nil)
+(defvar lcr-delay 0.2)
+
+(defun lcr-refresh ()
+  (when completion-in-region-mode
+    (completion-help-at-point)
+    (with-minibuffer-completions-window
+      (setq-local cursor-face-highlight-nonselected-window t)
+      (first-completion))))
+
+(defun lcr-after-change (beg end len)
+  (when completion-in-region-mode
+    (when lcr-timer
+      (cancel-timer lcr-timer))
+    (setf lcr-timer (run-at-time lcr-delay nil 'lcr-refresh))))
+
+(define-minor-mode lcr-mode
+  "Live Completion-In-Region Mode"
+  :global nil
+  (cond
+   (lcr-mode
+    (add-hook 'after-change-functions 'lcr-after-change nil t)
+    (add-hook 'completion-setup-hook 'minibuffer-next-completion nil t))
+   (t
+    (remove-hook 'after-change-functions 'lcr-after-change t)
+    (remove-hook 'completion-setup-hook 'minibuffer-next-completion t))))
