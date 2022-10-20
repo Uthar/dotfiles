fdfc599025d330157bf03c299d4f5227171a0d31
Kasper Gałkowski <k@galkowski.xyz>
2022-10-21 00:56:57 +0200

    usunąłem elpy

--- .config/emacs/modules/python-module.el
+++ .config/emacs/modules/python-module.el
@@ -1,24 +1,2 @@
 ;; -*- lexical-binding: t -*-
 
-(add-to-list 'load-path (concat +vendor-dir+ "elpy"))
-
-(autoload 'elpy-enable "elpy" "" t)
-(add-hook 'python-mode-hook 'elpy-enable)
-
-(setq elpy-modules '(
-                     elpy-module-sane-defaults
-                     elpy-module-company
-                     elpy-module-eldoc
-                     elpy-module-flymake
-                     ;; elpy-module-highlight-indentation
-                     ;; elpy-module-pyvenv
-                     ;; elpy-module-yasnippet
-                     ;; elpy-module-django
-                     ))
-
-;; Disable auto complete
-(add-hook 'elpy-mode-hook
-  (lambda ()
-    (setq-local company-idle-delay nil)
-    (local-set-key (kbd "M-.") 'elpy-goto-definition)))
-
