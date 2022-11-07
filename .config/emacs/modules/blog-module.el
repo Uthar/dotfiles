bdb037809c713ad90173de1441b8873591b45fee
Kasper Gałkowski <k@galkowski.xyz>
2022-11-07 01:37:50 +0100

    dodałem blog org publish

---
+++ .config/emacs/modules/blog-module.el
@@ -0,0 +1,27 @@
+;; -*- lexical-binding: t -*-
+
+(setq org-publish-project-alist
+      '(("posts"
+         :base-directory "~/blog/posts/"
+         :base-extension "org"
+         :publishing-directory "/ssh:jazajuk:/srv/blog/test/posts/"
+         :publishing-function org-html-publish-to-html
+         :section-numbers nil
+         :with-toc nil
+         :auto-sitemap t
+         :html-preamble nil
+         :html-doctype "html5"
+         :html-html5-fancy t
+         :html-self-link-headlines t
+         :html-validation-link nil)
+        ("images"
+         :base-directory "~/blog/images/"
+         :base-extension "jpg\\|gif\\|png"
+         :publishing-directory "/ssh:jazajuk:/srv/blog/test/images/"
+         :publishing-function org-publish-attachment)
+        ("files"
+         :base-directory "~/blog/files/"
+         :base-extension "css"
+         :publishing-directory "/ssh:jazajuk:/srv/blog/test/files/"
+         :publishing-function org-publish-attachment)
+        ("blog" :components ("posts" "images" "files"))))
