e88f4fe19c9fc7e9f1b76fe7a4b3cf7b7e618139
Kasper Gałkowski <k@galkowski.xyz>
2022-11-15 14:02:31 +0100

    kompletowanie trochę lepsze niż domyślnie:
    - kompletowanie plików przez cape-file
    - jedna kolumna propozycji
    - fix buga w emacsie 29.0.50 przy kompletowaniu w bufferach

---
+++ gnu/.config/emacs/modules/completion-module.el
@@ -0,0 +1,139 @@
+;; -*- lexical-binding: t -*-
+
+;;;;;;; Builtins for sanity
+
+(setopt completions-format 'one-column)
+(setopt completion-auto-help 'visible)
+(setopt completion-show-help nil)
+
+(setopt completion-ignore-case t)
+(setopt read-buffer-completion-ignore-case t)
+(setopt read-file-name-completion-ignore-case t)
+
+(add-to-list 'completion-styles 'flex)
+
+;;;;;;;; Limit completions buffer size
+
+(setq temp-buffer-max-height 15)
+(temp-buffer-resize-mode)
+
+;;;;;;;; Fix to screen jump in emacs 29-trunk
+
+(defun kaspi/minibuffer-choose-completion ()
+  (interactive)
+  (unwind-protect
+      (with-minibuffer-completions-window
+        (choose-completion))
+    (completion-in-region-mode -1)))
+
+(define-key completion-in-region-mode-map
+            (kbd "M-RET")
+            'kaspi/minibuffer-choose-completion)
+
+;;;;;;;; Add file CAPF
+
+(add-to-list 'load-path (concat +vendor-dir+ "cape"))
+(autoload 'cape-file "cape")
+(add-to-list 'completion-at-point-functions 'cape-file)
+
+;;;;;;;; Select first candidate on minibuffer completion
+
+;; (define-key minibuffer-local-must-match-map
+;;             (kbd "RET")
+;;             'minibuffer-complete-and-exit)
+
+;;;;;;;; Force select current text and exit
+
+;; (define-key minibuffer-local-must-match-map
+;;             (kbd "M-j")
+;;             'minibuffer-complete-and-exit)
+
+;;;;;;;; Switch to completions shortcut
+
+;; (define-key completion-in-region-mode-map (kbd "M-c") 'switch-to-completions)
+;; (define-key minibuffer-mode-map (kbd "M-c") 'switch-to-completions)
+
+
+;;;;;;;; Auto refresh completions buffer after typing
+
+;; (defvar lcr-timer nil)
+;; (defvar lcr-delay 0.01)
+
+;; (defvar lcr-minibuffer-commands
+;;   (list 'execute-extended-command
+;;         'switch-to-buffer
+;;         'find-file
+;;         'hexl-find-file
+;;         'describe-function
+;;         'describe-variable
+;;         'recentf
+;;         'recentf-open
+;;         'rename-file)
+;;   "Commands to allow refreshing completions after when in the minibuffer")
+
+;; (defvar lcr-minibuffer-disabled-commands
+;;   (list 'query-replace
+;;         'query-replace-regexp)
+;;   "Minibuffer commands to not refresh completions for")
+
+;; (defvar lcr-commands
+;;   (list 'self-insert-command
+;;         'delete-backward-char
+;;         'backward-delete-char-untabify)
+;;   "Commands to trigger completion help after, whether in region or minibuffer")
+
+;; (defun lcr-refresh ()
+;;   (cond
+;;    ((and (minibufferp)
+;;          (not (memq current-minibuffer-command
+;;                     lcr-minibuffer-disabled-commands)))
+;;     (let ((minibuffer-completion-auto-choose nil))
+;;       (minibuffer-completion-help)
+;;       (minibuffer-next-completion)))
+;;    (completion-in-region-mode
+;;     (completion-help-at-point)
+;;     (minibuffer-next-completion))))
+
+;; (defun lcr-after-change (&rest _)
+;;   ;; (message "minibuffer entered from %s" current-minibuffer-command)
+;;   (when lcr-timer
+;;     (cancel-timer lcr-timer))
+;;   ;; (message "this-command %s" this-command)
+;;   (when (and (memq this-command lcr-commands)
+;;              (or completion-in-region-mode
+;;                  (minibufferp)))
+;;     ;; (message "going after %s" this-command)
+;;     (setf lcr-timer (run-at-time lcr-delay nil 'lcr-refresh))))
+
+;; (define-minor-mode global-lcr-mode
+;;   "Live Completion-In-Region Mode"
+;;   :global t
+;;   (cond
+;;    (global-lcr-mode
+;;     (add-hook 'post-command-hook 'lcr-after-change))
+;;    (t
+;;     (remove-hook 'post-command-hook 'lcr-after-change))))
+
+;; (advice-add 'completion-at-point
+;;             :after
+;;             (lambda (&rest _) (minibuffer-next-completion))
+;;             '((name . next-completion)))
+
+;;             
+;; (advice-remove 'completion-at-point 'next-completion)
+
+;; (define-key completion-in-region-mode-map (kbd "C-n") 'minibuffer-next-completion)
+;; (define-key completion-in-region-mode-map (kbd "C-p") 'minibuffer-previous-completion)
+
+
+
+;; (define-key minibuffer-mode-map (kbd "C-n") 'minibuffer-next-completion)
+;; (define-key minibuffer-mode-map (kbd "C-p") 'minibuffer-previous-completion)
+;; (define-key minibuffer-mode-map (kbd "M-c") 'switch-to-completions)
+
+;; (define-key minibuffer-local-must-match-map (kbd "RET") 'kaspi/minibuffer-choose-completion)
+
+
+
+
+;; (add-hook 'after-init-hook 'global-lcr-mode)
