324b1a275c68879613628697ffeffa2748a96bbb
Kasper Gałkowski <k@galkowski.xyz>
2022-11-15 18:34:31 +0100

    now also for rg

--- gnu/.config/emacs/modules/search-module.el
+++ gnu/.config/emacs/modules/search-module.el
@@ -1,49 +1,55 @@
 ;; -*- lexical-binding: t -*-
 
-(defun kaspi/call-with-minibuffer-contents (command transform)
-  (interactive)
-  (when (and (minibufferp) (not (string-empty-p (minibuffer-contents))))
-    (funcall command (funcall transform (minibuffer-contents)))))
+(defun kaspi/call-for-live-compile (fn)
+  (lambda ()
+    (let ((contents (minibuffer-contents)))
+      (when (and (minibufferp)
+                 (not (string-empty-p contents))
+                 (memq this-command lcr-commands))
+        (funcall fn contents)))))
 
 (defun kaspi/live-compile (query fn)
   (interactive)
-  (advice-add 'sit-for :override (cl-constantly nil) '((name . noop)))
-  (setopt compilation-always-kill t)
-  (add-hook 'post-command-hook fn)
-  (unwind-protect
-      (let ((inhibit-message t))
-        (read-string query))
-    (remove-hook 'post-command-hook fn)
-    (setopt compilation-always-kill nil)
-    (advice-remove 'sit-for 'noop)))
-
-(defun kaspi/fd ()
-  (kaspi/run-with-minibuffer-contents 'fd 'identity))
-
-(defun kaspi/fd2 ()
-  (interactive)
-  (kaspi/live-compile "find file: " 'kaspi/fd))
+  (let ((hook (kaspi/call-for-live-compile fn)))
+    ;; compile mode has hard coded sleep after command termination
+    (advice-add 'sit-for :override (cl-constantly nil) '((name . noop)))
+    (setopt compilation-always-kill t)
+    (add-hook 'post-command-hook hook)
+    (unwind-protect
+        (let ((inhibit-message t))
+          (read-string query))
+      (remove-hook 'post-command-hook hook)
+      (setopt compilation-always-kill nil)
+      (advice-remove 'sit-for 'noop))))
 
 (defvar fd-last-buffer nil)
 
 (defvar fd-regexp-alist
-  '(("^\\(.*\\)$" 1 nil nil 0)))
+  '(("^\\(.+\\)$" 1)))
 
 (require 'compile)
+(require 'dired)
 
 (define-compilation-mode fd-mode "fd"
   "Mode to find files with the fd program"
   (setq fd-last-buffer (current-buffer))
   (setq-local compilation-error-regexp-alist
-              fd-regexp-alist))
+              fd-regexp-alist)
+  (setq-local compilation-error-face
+              dired-symlink-face))
 
-(defun fd (regex)
+(defun kaspi/fd (regex)
   (interactive)
   (compilation-start (format "fd --color=never %s" regex) #'fd-mode))
 
+(defun kaspi/fd2 ()
+  (interactive)
+  (kaspi/live-compile "find file: " 'kaspi/fd))
 
+(defun kaspi/rg (regex)
+  (interactive)
+  (rg regex "all" default-directory))
 
-
-
-
- 
+(defun kaspi/rg2 ()
+  (interactive)
+  (kaspi/live-compile "grep for: " 'kaspi/rg))
