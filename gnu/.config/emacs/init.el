ffdcc8da075d6bf63faac45d21bc37a15564553f
Kasper Gałkowski <kgalkowski@comscore.com>
2023-03-14 13:15:20 +0100

    Notatka jak zwiększyć rozmiar fontu

--- gnu/.config/emacs/init.el
+++ gnu/.config/emacs/init.el
@@ -72,3 +72,6 @@
 ;; C-x e  call kb macro
 ;; j in dired, fast go to file or directory by name
 ;; C-x v C-h - show keybindings starting with C-x v (can be whatever)
+
+;; Dla ekranów z wysoką rozdzielczością:
+;; (set-face-attribute 'default nil :height 130)
