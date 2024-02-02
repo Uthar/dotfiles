2ba45a4046b29ef6e4a65fce5f13d6e52c332120
Kasper Gałkowski <kgalkowski@comscore.com>
2024-02-02 19:18:01 +0100

    fix todo regexps for common lisp symbols which are uppercased
    This should work for note-foo etc.

--- gnu/.config/emacs/modules/todo-module.el
+++ gnu/.config/emacs/modules/todo-module.el
@@ -3,8 +3,8 @@
 (defun kaspi/enable-todo-highlighting ()
   (interactive)
   (font-lock-add-keywords nil
-      '(("\\<TODO\\>"  0 '(:foreground "#e5534b" :weight bold) t)
-        ("\\<FIXME\\>" 0 '(:foreground "#e0823d" :weight bold) t)
-        ("\\<NOTE\\>"  0 '(:foreground "#57ab5a" :weight bold) t))))
+      '(("\\<TODO\\>[^-]"  0 '(:foreground "#e5534b" :weight bold) t)
+        ("\\<FIXME\\>[^-]" 0 '(:foreground "#e0823d" :weight bold) t)
+        ("\\<NOTE\\>[^-]"  0 '(:foreground "#57ab5a" :weight bold) t))))
 
 (add-hook 'prog-mode-hook 'kaspi/enable-todo-highlighting)
