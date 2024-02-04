62cf771cb33a28b2ec358680ea97b47118ec8216
Kasper Gałkowski <k@galkowski.xyz>
2024-02-04 01:29:57 +0100

    dodane podświatlanie BUG

--- gnu/.config/emacs/modules/todo-module.el
+++ gnu/.config/emacs/modules/todo-module.el
@@ -1,10 +1,22 @@
 ;; -*- lexical-binding: t -*-
 
+(defvar kaspi/todo-font-lock-keywords
+  '(("\\<TODO\\>"  0 '(:foreground "#e5534b" :weight bold) t)
+    ("\\<FIXME\\>" 0 '(:foreground "#e0823d" :weight bold) t)
+    ("\\<NOTE\\>"  0 '(:foreground "#57ab5a" :weight bold) t)
+    ("\\<BUG\\>"   0 '(:foreground "#9b30ff" :weight bold) t)))
+
+;; TODO
+;; FIXME
+;; NOTE
+;; BUG
+
+(defun kaspi/disable-todo-highlighting ()
+  (interactive)
+  (font-lock-remove-keywords nil kaspi/todo-font-lock-keywords))
+
 (defun kaspi/enable-todo-highlighting ()
   (interactive)
-  (font-lock-add-keywords nil
-      '(("\\<TODO\\>[^-]"  0 '(:foreground "#e5534b" :weight bold) t)
-        ("\\<FIXME\\>[^-]" 0 '(:foreground "#e0823d" :weight bold) t)
-        ("\\<NOTE\\>[^-]"  0 '(:foreground "#57ab5a" :weight bold) t))))
+  (font-lock-add-keywords nil kaspi/todo-font-lock-keywords))
 
 (add-hook 'prog-mode-hook 'kaspi/enable-todo-highlighting)
