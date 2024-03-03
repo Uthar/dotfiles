10c3c1e142f53df4cc6379e3cc3b12c340e311b8
Kasper Gałkowski <kgalkowski@comscore.com>
2024-03-04 00:18:14 +0100

    Dodana komenda x w ediff - połącz A i B do C

--- gnu/.config/emacs/modules/diff-module.el
+++ gnu/.config/emacs/modules/diff-module.el
@@ -14,3 +14,15 @@
 
 ;; Better for wide screens which I use
 (setq ediff-split-window-function 'split-window-horizontally)
+
+;; Combine A and B during merge
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+    (concat 
+     (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+     (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+
+(add-hook 'ediff-keymap-setup-hook
+  (lambda ()
+    (define-key ediff-mode-map (kbd "x") 'ediff-copy-both-to-C)))
