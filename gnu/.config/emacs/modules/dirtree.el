4ad32e0a249e5af8c3949971535f90f26c3b4673
Kasper Gałkowski <kgalkowski@comscore.com>
2024-06-13 00:33:37 +0200

    Dirtree scratch
    - dodałem chowanie/pokazywanie zawartości folderów
    - dodałem odświeżanie całego drzewka

--- gnu/.config/emacs/modules/dirtree.el
+++ gnu/.config/emacs/modules/dirtree.el
@@ -3,6 +3,7 @@
 ;;;; File-browser
 
 (require 'eieio)
+(require 'subr-x)
 
 (defclass dirtree-node ()
   ((path
@@ -58,7 +59,7 @@
 (.children (make-instance 'dirtree-node :path "~/.bashrc"))
 
 (defun dirtree-node-at-point ()
-  (get-text-property (point) 'dirtree-node))
+  (get-text-property (line-beginning-position) 'dirtree-node))
 
 (defun dirtree-expand ()
   (interactive)
@@ -88,19 +89,28 @@
                                 :kind (dirtree-decode-kind dirp))
                  children)))
        (setf (.children node) children)
-       (end-of-line)
-       (newline)
-       (setf start (point))
-       (cl-loop for node in children do
-                (insert (propertize (format "%s%s: %s\n"
-                                            (make-string (.depth node) ?-)
-                                            (.kind node)
-                                            (file-name-nondirectory (.path node)))
-                                    'dirtree-node node)))
-       (setf end (point))
+       (save-excursion
+         (end-of-line)
+         (newline)
+         (setf start (point))
+         (cl-loop for node in children do
+                  ;; TODO defmethod dirtree-format-node
+                  (insert (propertize (format "%s%s: %s\n"
+                                              (make-string (.depth node) ?\ )
+                                              (.kind node)
+                                              (file-name-nondirectory (.path node)))
+                                      'dirtree-node node)))
+         ;; Always because it removes the initial newline, which is unneeded if
+         ;; there were children, because they always write a trailing newline,
+         ;; and it is unneeded when there were no children, because it creates
+         ;; an extra empty line.
+         (when t ;(consp children)
+           (backward-delete-char 1))
+         (setf end (1+ (point))))
        (setf (.subtree-overlay node) (make-overlay start end))
        (setf (.subtree-state node) :opened)
-       ;; (overlay-put (.subtree-overlay node) 'face 'highlight)
+       ;; For debugging
+       ;; (overlay-put (.subtree-overlay node) 'dirtree-path (.path node))
        ))
     (:opened
      (overlay-put (.subtree-overlay node) 'invisible t)
@@ -123,10 +133,101 @@
   (interactive)
   (delete-window))
 
-(defun dirtree-refresh ()
+;; (defun dirtree--refresh-subtree (node)
+;;   (cl-loop for sub in (.children node) do
+;;     ;; Only refresh inited dirs.
+;;     ;; Init empty dirs too, because files could have been added to it.
+;;     ;;
+;;     ;; I'll refresh all initialized subtrees (including closed) in order to not
+;;     ;; have to refresh those lazily. It's slower but gives simpler code.
+;;     ;;
+;;     ;; I have to work my way up from the deepest inited nodes so that deleting
+;;     ;; lines does not mess up overlays.
+;;     (when (and (eq :dir (.kind sub))
+;;                (memq (.subtree-state sub) '(:opened :closed)))
+;;       (dirtree--refresh-subtree sub))
+;;     ;; After processing all the child nodes, we can refresh our own ls state.
+;;     (cl-adjoin '("foo") '(("a") ("b") ("foo")) :test #'string= :key #'car)
+
+;;     (setf ___tmp123 (list '("a") '("b") '("foo")))
+;;     (cl-pushnew '("bar") ___tmp123 :test #'string= :key #'car)
+
+
+(defun dirtree--refresh-subtree-2 (node)
+    ;; ALTERNATIVE SOLUTION
+    ;; First remember which nodes were opened.
+    ;; Then wipe all nodes
+    ;; Reinit root then reinit the remembered nodes, too.
+    (let* ((oldroot *dirtree-root*)
+           (newroot (make-instance 'dirtree-node :path (.path oldroot))))
+      (erase-buffer)
+      (cl-labels
+        ((reopen (node)
+           (when-let
+               ((savedroot
+                 (cl-find-if
+                  (lambda (oldnode)
+                    (and (eq :dir (.kind oldnode))
+                         (eq :opened (.subtree-state oldnode))
+                         (string= (.path oldnode) (.path node))))
+                  (.children *dirtree-root*))))
+             (dirtree-expand-dir node)
+             ;; (pulse-momentary-highlight-one-line)
+             ;; (sit-for 0.5)
+             (let ((*dirtree-root* savedroot))
+               (cl-loop for child in (.children node) do
+                 (dirtree-next)
+                 (reopen child))))))
+        (let ((*dirtree-root* (make-instance 'dirtree-node :path null-device)))
+          (setf (.children *dirtree-root*) (list oldroot))
+          (reopen newroot))
+        ;; Save new root with recreated structure
+        (setq-local *dirtree-root* newroot))))
+
+(defun dirtree--DEBUG-visualise-children ()
   (interactive)
-  (erase-buffer)
-  (dirtree-expand-dir *dirtree-root*))
+  ;; Only inited dirs have the overlay. It tracks their position.
+  ;;
+  ;; A node can only get inited when it has an inited parent, so we can walk
+  ;; down starting from root.
+  ;;
+  ;; Let's jump to each such dir and refresh their children, starting from the
+  ;; deepes child.
+  (cl-loop for sub in (.children (dirtree-node-at-point)) do
+    (when (slot-boundp sub 'subtree-overlay)
+      (goto-char (1- (overlay-start (.subtree-overlay sub))))
+      (sit-for 0.5)
+      (pulse-momentary-highlight-one-line)
+      (dirtree--DEBUG-visualise-children))))
+                 
+(defun dirtree-refresh ()
+  "Refresh opened subtrees to detect added and deleted files."
+  (interactive)
+  ;; Remember it to bring the point back to it after refresh.
+  (let ((saved-node (dirtree-node-at-point)))
+    (dirtree--refresh-subtree-2 *dirtree-root*)
+    (goto-char (point-min))
+    (dirtree-next)
+    (cl-loop for node = (dirtree-node-at-point) while node
+             until (string= (.path node) (.path saved-node))
+             do (dirtree-next))
+    ;; (recenter)
+    ;; TODO żeby przywracał też scroll (scroll-excursion?)
+    (pulse-momentary-highlight-one-line)))
+
+            
+        
+    
+
+  
+;; I'll refresh all initialized subtrees (including closed) in order to not
+;; have to refresh those lazily. It's slower but gives simpler code.
+;;
+;; I have to work my way up from the deepest inited nodes so that deleting
+;; lines does not mess up overlays.
+;;
+;; After processing all the child nodes, we can refresh our own ls state.
+
 
 (defun dirtree-previous ()
   (interactive)
@@ -160,6 +261,11 @@
            until (= (1- depth) (.depth node))
            do (dirtree-previous)))
 
+(defun dirtree-copy-path ()
+  "Save full path of file under point to kill ring."
+  (interactive)
+  (kill-new (message (.path (dirtree-node-at-point)))))
+
 (defvar-keymap dirtree-mode-map
   :doc "Keymap for File Browser mode"
   "<return>" #'dirtree-expand
@@ -169,9 +275,14 @@
   "}" #'dirtree-next-dir
   "^" #'dirtree-up-dir
   "q" #'dirtree-close-dirtree
+  "w" #'dirtree-copy-path
   "g" #'dirtree-refresh)
 
+;; TODO isearch?
+
 (define-derived-mode dirtree-mode nil "File Browser"
-  ;; (setq-local *dirtree-root* (make-instance 'dirtree-node :path default-directory))
-  (setq-local *dirtree-root* (make-instance 'dirtree-node :path "~/Repos"))
-  (dirtree-expand-dir *dirtree-root*))
+  (setq-local *dirtree-root* (make-instance 'dirtree-node :path (string-remove-suffix "/" default-directory)))
+  ;; (setq-local *dirtree-root* (make-instance 'dirtree-node :path "~/Repos"))
+  (dirtree-expand-dir *dirtree-root*)
+  ;; TODO read-only-mode
+  )
