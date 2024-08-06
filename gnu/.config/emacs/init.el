3c31e4f576dba11a3a5bdfcbf16b31f1fc3dae9e
Kasper Gałkowski <galkowskikasper@gmail.com>
2024-08-07 00:13:12 +0200

    ارقام

--- gnu/.config/emacs/init.el
+++ gnu/.config/emacs/init.el
@@ -117,3 +117,10 @@
 ;; (add-hook 'completion-list-mode-hook 'toggle-truncate-lines)
 ;; (marginalia-mode 'toggle)
 
+;; ٠١٢٣٤٥٦٧٨٩
+(with-eval-after-load "quail/arabic"
+  (let ((quail-current-package (assoc "arabic" quail-package-alist)))
+    (quail-define-rules
+     ((append . t))
+     ("0" "\u0660") ("1" "\u0661") ("2" "\u0662") ("3" "\u0663") ("4" "\u0664")
+     ("5" "\u0665") ("6" "\u0666") ("7" "\u0667") ("8" "\u0668") ("9" "\u0669"))))
