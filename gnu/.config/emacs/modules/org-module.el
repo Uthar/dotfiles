316fc02f9a0eba10b0c8049a0557cfbe75e43911
Kasper Gałkowski <galkowskikasper@gmail.com>
2024-10-05 00:29:39 +0200

    Ustawienia org-mode i kalendarza

--- gnu/.config/emacs/modules/org-module.el
+++ gnu/.config/emacs/modules/org-module.el
@@ -11,3 +11,27 @@
 ;; For <q TAB and <s TAB spinnets
 (with-eval-after-load "org"
   (require 'org-tempo))
+
+(setopt org-agenda-files '("~/Org/"))
+(setopt org-agenda-span 20)
+
+(with-eval-after-load "org"
+  (make-directory "~/Org/" t))
+
+;; Poniedziałek
+(setopt calendar-week-start-day 1)
+
+(setopt zoneinfo-style-world-list
+  '(("UTC" "UTC")
+    ("Europe/Oslo" "Oslo")
+    ;; ("Europe/Athens" "Ateny")
+    ;; ("Japan" "Tokio")
+    ;; ("US/Pacific" "Seattle")
+    ;; ("America/New_York" "Nowy Jork")
+    ;; ("US/Eastern" "Virginia")
+    ;; ("US/Central" "Texas")
+    ;; ("Asia/Kolkata" "Indie")
+    ;; ("Japan" "Tokio")
+    ;; ("America/Sao_Paulo" "Sao Paulo")
+    ;; ("Chile/Continental" "Santiago")
+    ))
