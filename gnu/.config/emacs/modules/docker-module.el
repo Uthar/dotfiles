92e0f2875ec60cdc11faa6fdc1e0e1fb2d8b6098
Kasper Gałkowski <galkowskikasper@gmail.com>
2024-11-08 01:00:51 +0100

    usunięty nieużywany kubel

--- gnu/.config/emacs/modules/docker-module.el
+++ gnu/.config/emacs/modules/docker-module.el
@@ -3,8 +3,6 @@
 (add-to-list 'load-path (concat +vendor-dir+ "dockerfile-mode"))
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
 (autoload 'dockerfile-mode "dockerfile-mode" "" t)
-(add-to-list 'load-path (concat +vendor-dir+ "kubel"))
-(autoload 'kubel "kubel" "" t)
 
 ;; Z jakiegoś powodu tramp się wysypuje przy edytowaniu plików w kontenerach...
 (advice-add 'tramp-get-remote-path :filter-return 
